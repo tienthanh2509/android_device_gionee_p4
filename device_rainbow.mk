46d6e70ff0cec21a36d3474cbfdb28fe3f2bc8b1
Al3XKOoL <moonrotation@gmail.com>
2015-04-02 23:21:35 +0100

    ramdisk/add bt

--- device_rainbow.mk
+++ device_rainbow.mk
@@ -16,6 +16,8 @@ else
 	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
 endif
 
+PRODUCT_TAGS += dalvik.gc.type-precise
+
 PRODUCT_PACKAGES += \
     libxlog
 
@@ -25,14 +27,15 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libmtkrilw
 
+# audio
 PRODUCT_PACKAGES += \
-    audio.r_submix.default
-
-PRODUCT_PACKAGES += \
-    audio.primary.mt6582
-
-PRODUCT_PACKAGES += \
-    audio_policy.default
+    audio.r_submix.default \
+    audio.a2dp.default \
+    audio.primary.mt6582 \
+    audio.usb.default \
+    libaudio-resampler \
+    audio_policy.default \
+    tinymix
 
 PRODUCT_PACKAGES += \
     lib_driver_cmd_mt66xx
@@ -41,26 +44,42 @@ PRODUCT_COPY_FILES += \
     device/wiko/rainbow/mtk-kpd.kl:system/usr/keylayout/mtk-kpd.kl
 
 PRODUCT_COPY_FILES += \
-    device/wiko/rainbow/init.recovery.mt6582.rc:root/init.recovery.mt6582.rc
-
-PRODUCT_COPY_FILES += \
-    device/wiko/rainbow/audio/audio_policy.conf:system/etc/audio_policy.conf
-
-PRODUCT_COPY_FILES += \
-    device/wiko/rainbow/fstab.mt6582:root/fstab.mt6582 \
-    device/wiko/rainbow/init.mt6582.rc:root/init.mt6582.rc \
+    device/wiko/rainbow/root/fstab.mt6582:root/fstab.mt6582 \
+    device/wiko/rainbow/root/init.mt6582.rc:root/init.mt6582.rc \
+    device/wiko/rainbow/root/init.recovery.mt6582.rc:root/init.recovery.mt6582.rc \
+    device/wiko/rainbow/root/init.protect.rc:root/init.protect.rc \
+    device/wiko/rainbow/root/init.project.rc:root/init.project.rc \
+    device/wiko/rainbow/root/init.usb.rc:root/init.usb.rc \
     $(LOCAL_KERNEL):kernel
 
 PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
 	frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
-	frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
-	frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
+	frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
+	frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
+	frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+	frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+	frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+	frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+	frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+	frameworks/native/data/etc/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
+	frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+	frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml
 
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/media_codecs.xml:system/etc/media_codecs.xml \
+	$(LOCAL_PATH)/media_profiles.xml:system/etc/media_profiles.xml
 
 $(call inherit-product, build/target/product/full.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+PRODUCT_PROPERTY_OVERRIDES := \
+	ro.mediatek.version.release=ALPS.KK1.MP1.V2.10 \
+	ro.mediatek.platform=MT6582 \
+	ro.mediatek.chip_ver=S01 \
+	ro.mediatek.version.branch=KK1.MP1 \
+	ro.mediatek.version.sdk=2 \
+	persist.mtk.wcn.combo.chipid=-1
+
 PRODUCT_NAME := full_rainbow
 PRODUCT_DEVICE := rainbow
 
