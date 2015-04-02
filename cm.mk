7c915c0dd14676373a35715f27ed4ccc1c284d5f
Al3XKOoL <moonrotation@gmail.com>
2015-04-02 12:43:58 +0100

    Boot animation resolution

--- cm.mk
+++ cm.mk
@@ -10,6 +10,10 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Inherit device configuration
 $(call inherit-product, device/wiko/rainbow/device_rainbow.mk)
 
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := rainbow
 PRODUCT_NAME := cm_rainbow
